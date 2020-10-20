57d32f0159d11ca485515e281cf3e3b09a91191b
Ashton Chen <cdljsj@gmail.com>
2020-10-20 09:50:17 +0800

    Minor update for truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -19,8 +19,6 @@
  */
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
 
@@ -69,14 +67,15 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-      kovan: {
-          provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/98520c87aac949ac9c2310084b2d48b8`),
-          network_id: 42,       // Ropsten's id
-          gas: 5500000,        // Ropsten has a lower block limit than mainnet
-          confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-          timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-          skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-      },
+
+    kovan: {
+        provider: () => new HDWalletProvider(mnemonic, `https://kovan.infura.io/v3/98520c87aac949ac9c2310084b2d48b8`),
+        network_id: 42,       // Ropsten's id
+        gas: 5500000,        // Ropsten has a lower block limit than mainnet
+        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
@@ -92,6 +91,7 @@ module.exports = {
           skipDryRun: true,
           networkCheckTimeout: 100000000
       },
+
       elaeth: {
           provider: () => new HDWalletProvider(mnemonic, `https://mainrpc.elaeth.io`),
           network_id: "1",   // This network is yours, in the cloud.
