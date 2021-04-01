a3951b1a2df39a896e17ce8f2f9e92e9e6405041
Ashton Chen <cdljsj@gmail.com>
2021-04-01 17:30:25 +0800

    format interest rate display

--- scripts/viewInterestModel.js
+++ scripts/viewInterestModel.js
@@ -8,18 +8,21 @@ module.exports = async function(callback) {
         let unitrollerInstance = await Unitroller.deployed();
         let proxiedQstroller = await Qstroller.at(unitrollerInstance.address);
         let allSupportedMarkets = await proxiedQstroller.getAllMarkets();
+        // let allSupportedMarkets = ["0x0d37214e9A4B6E1AE3bA664bac3A6f0cbDa06665","0x0b3f6F8B72011F9Af56bfF9cd785633cC3BbEf18"];
         for (market of allSupportedMarkets) {
             let cTokenInstance = await CToken.at(market);
             let cTokenName = await cTokenInstance.name();
             let compSpeed = await proxiedQstroller.compSpeeds(market);
             if (compSpeed <= 0) continue;
+            let reserveFactorMantissa = await cTokenInstance.reserveFactorMantissa() / Math.pow(10, 18);
             let interestRateModel = await cTokenInstance.interestRateModel();
             let interestRateModelInstance = await InterestModel.at(interestRateModel);
             let blocksPerYear = await interestRateModelInstance.blocksPerYear();
             let multiplierPerBlock = await interestRateModelInstance.multiplierPerBlock();
             let baseRatePerBlock = await interestRateModelInstance.baseRatePerBlock();
             let jumpMultiplierPerBlock = await interestRateModelInstance.jumpMultiplierPerBlock();
-            console.log(`${cTokenName} ${market} interestModel: ${interestRateModel} blocksPerYear: ${blocksPerYear} multiplierPerBlock: ${multiplierPerBlock} baseRatePerBlock: ${baseRatePerBlock} jumpMultiplierPerBlock: ${jumpMultiplierPerBlock}`);
+            let kink = await interestRateModelInstance.kink();
+            console.log(`${cTokenName} ${market} reserveFactorMantissa: ${reserveFactorMantissa.toFixed(2)} interestModel: ${interestRateModel} blocksPerYear: ${blocksPerYear} baseRatePerBlock: ${(baseRatePerBlock * blocksPerYear/Math.pow(10, 18)).toFixed(2)} multiplierPerBlock: ${(multiplierPerBlock * blocksPerYear/Math.pow(10, 18)).toFixed(2)} kink: ${kink/Math.pow(10, 18)} jumpMultiplierPerBlock: ${(jumpMultiplierPerBlock * blocksPerYear/Math.pow(10, 18)).toFixed(2)}`);
         }
         callback();
     } catch (e) {
