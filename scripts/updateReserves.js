12e38425c38b699454919706696a41188c2b8b98
Ashton Chen <cdljsj@gmail.com>
2021-04-10 15:30:01 +0800

    update CompSpeed and reserve

--- scripts/updateReserves.js
+++ scripts/updateReserves.js
@@ -1,13 +1,13 @@
 const Qstroller = artifacts.require("Qstroller");
 const CToken = artifacts.require("CToken");
 const Unitroller = artifacts.require("Unitroller");
-const reserveFactor = 0.15e18.toString();
+const reserveFactor = 0.12e18.toString();
 module.exports = async function(callback) {
     try {
         let unitrollerInstance = await Unitroller.deployed();
         let proxiedQstroller = await Qstroller.at(unitrollerInstance.address);
         //let allSupportedMarkets = await proxiedQstroller.getAllMarkets();
-        let allSupportedMarkets = ["0x5788C014D41cA706DE03969E283eE7b93827B7B1"]
+        let allSupportedMarkets = ["0xAab0C9561D5703e84867670Ac78f6b5b4b40A7c1"]
         for (market of allSupportedMarkets) {
             let cTokenInstance = await CToken.at(market);
             let compSpeed = await proxiedQstroller.compSpeeds(market);
