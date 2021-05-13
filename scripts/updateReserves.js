c1d2f8f85cbcf16ea92a2b65052af009c63c1405
Ashton Chen <cdljsj@gmail.com>
2021-05-13 16:33:36 +0800

    update husd reserve ratio to 12%

--- scripts/updateReserves.js
+++ scripts/updateReserves.js
@@ -7,7 +7,7 @@ module.exports = async function(callback) {
         let unitrollerInstance = await Unitroller.deployed();
         let proxiedQstroller = await Qstroller.at(unitrollerInstance.address);
         //let allSupportedMarkets = await proxiedQstroller.getAllMarkets();
-        let allSupportedMarkets = ["0xAab0C9561D5703e84867670Ac78f6b5b4b40A7c1"]
+        let allSupportedMarkets = ["0xB16Df14C53C4bcfF220F4314ebCe70183dD804c0"]
         for (market of allSupportedMarkets) {
             let cTokenInstance = await CToken.at(market);
             let compSpeed = await proxiedQstroller.compSpeeds(market);
