31cad1d59cf7570681bcbcebf4cff493252b7d87
Ashton Chen <cdljsj@gmail.com>
2021-06-15 10:03:10 +0800

    update reserve ratio for specific pools

--- scripts/updateReserves.js
+++ scripts/updateReserves.js
@@ -1,13 +1,13 @@
 const Qstroller = artifacts.require("Qstroller");
 const CToken = artifacts.require("CToken");
 const Unitroller = artifacts.require("Unitroller");
-const reserveFactor = 0.2e18.toString();
+const reserveFactor = 0.3e18.toString();
 module.exports = async function(callback) {
     try {
         let unitrollerInstance = await Unitroller.deployed();
         let proxiedQstroller = await Qstroller.at(unitrollerInstance.address);
         //let allSupportedMarkets = await proxiedQstroller.getAllMarkets();
-        let allSupportedMarkets = ["0xAab0C9561D5703e84867670Ac78f6b5b4b40A7c1"]
+        let allSupportedMarkets = ["0xB16Df14C53C4bcfF220F4314ebCe70183dD804c0"]
         for (market of allSupportedMarkets) {
             let cTokenInstance = await CToken.at(market);
             let compSpeed = await proxiedQstroller.compSpeeds(market);
