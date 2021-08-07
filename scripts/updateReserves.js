a214e8b851dc291548c9164176ca2e55e0fb8943
Ashton Chen <cdljsj@gmail.com>
2021-08-07 23:59:59 +0800

    update scripts

--- scripts/updateReserves.js
+++ scripts/updateReserves.js
@@ -1,13 +1,14 @@
 const Qstroller = artifacts.require("Qstroller");
 const CToken = artifacts.require("CToken");
 const Unitroller = artifacts.require("Unitroller");
-const reserveFactor = 0.35e18.toString();
+const reserveFactor = 0.5e18.toString();
 module.exports = async function(callback) {
     try {
         let unitrollerInstance = await Unitroller.deployed();
         let proxiedQstroller = await Qstroller.at(unitrollerInstance.address);
         //let allSupportedMarkets = await proxiedQstroller.getAllMarkets();
         let allSupportedMarkets = ["0xAab0C9561D5703e84867670Ac78f6b5b4b40A7c1","0xB16Df14C53C4bcfF220F4314ebCe70183dD804c0"]
+        //let allSupportedMarkets = ["0xF173F3897753692E7465E0932fe2285707E7E609"]
         for (market of allSupportedMarkets) {
             let cTokenInstance = await CToken.at(market);
             let compSpeed = await proxiedQstroller.compSpeeds(market);
